c94daf4c813c3993a17565debc49e950ba963c62
Eric <ricrow@localhost.localdomain>
2017-04-29 10:21:52 -0400

    wxPrac1

--- wxPractice/wxPrac1.py
+++ wxPractice/wxPrac1.py
@@ -1,4 +1,11 @@
 import wx
-app = wx.App()
-print("You are Bad")
-print("Git Works!")
+
+class garbo(wx.Frame):
+	def __init__(self,parent,id):
+		wx.Frame.__init__(self,parent,id,'Frame/Window', size=(300,200))
+
+if __name__=='__main__':
+	app = wx.App()
+	frame = garbo(parent=None, id=-1)
+	frame.Show()
+	app.MainLoop()
